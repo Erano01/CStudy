045d3ff890c1333d40db601cf6a9330ed7a70ca5
Erano01 <muhammedalisert01@gmail.com>
2026-08-17 02:04:51 +0300

    Feat: readme update

--- understanding-and-using-c-pointers-reese/ch1-pointer-foundations/src/pointers-and-memory.c
+++ understanding-and-using-c-pointers-reese/ch1-pointer-foundations/src/pointers-and-memory.c
@@ -3,5 +3,10 @@
 
 int main(int argc, char const *argv[]) {
     
+
+    char *names[] = {"Miller","Jones","Anderson"};
+    printf("%c\n",*(*(names+1)+2));
+    printf("%c\n",names[1][2]);
+
     return 0;
 }
